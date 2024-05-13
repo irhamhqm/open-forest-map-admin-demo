10d1468d1fe7d03a7f90d2efab45ade42c45c68b
bamzato <44289607+bamzato@users.noreply.github.com>
2024-05-13 08:42:27 +0700

    Perbaikan Fitur Halaman Signup (#4)
    * User Login And User Sign Up
    
    * Halaman Sign Up
    
    * Missed Something on Sign Up
    
    * Bug Fixing Little Thing

--- src/types/api/auth.ts
+++ src/types/api/auth.ts
@@ -1,10 +1,35 @@
+import { IsMeData } from "..";
+
 export interface SignInPayload {
     user_name: string;
     user_password: string;
   }
   
-  export interface SignInResponse {
-    data: string;
-    meta: string;
-    status: true;
-  }
+export interface SignInResponse {
+  data: string;
+  meta: string;
+  status: boolean;
+}
+
+export interface IsMeResponse {
+  data: IsMeData | undefined;
+  meta: string;
+  status: boolean;
+}
+
+export interface SignUpPayload {
+  user_name: string;
+  user_email: string;
+  user_password: string;
+  user_display_name: string;
+  user_affiliation: string;
+  user_role: string;
+  pilot_id: string;
+  feature_ids: string;
+}
+
+export interface SignUpResponse {
+  data: unknown;
+  meta: string;
+  status: boolean;
+}
