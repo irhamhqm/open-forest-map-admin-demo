856c5b43123260729bf1d36e2a49dec221fb2b46
bamzato <bamzato92.bp@gmail.com>
2024-05-15 20:54:07 +0700

    Erase unused code

--- src/hooks/api/auth.ts
+++ src/hooks/api/auth.ts
@@ -1,24 +1,11 @@
 import { useMutation, useQuery } from "@tanstack/react-query";
 import { SignInPayload, SignUpPayload } from "../../types/api/auth";
 import { signIn, getIsMe, signUp } from "../../api/auth";
-import axios, { AxiosError } from 'axios';
 
 export const useSignIn = () => {
     return useMutation({
-      mutationKey: ["login"],
+      // mutationKey: ["login"],
       mutationFn: (payload: SignInPayload) => signIn(payload),
-      onError: (error: AxiosError | Error) => {
-        if(!('isAxiosError' in error)) {
-          console.log('Non-Axios error:', error.message);
-          return;
-        }
-
-        if(error.response) {
-          console.error('Error response:', error.response.data);
-        } else {
-          console.error('Network error:', error.message);
-        }
-      },
     });
   };
 
